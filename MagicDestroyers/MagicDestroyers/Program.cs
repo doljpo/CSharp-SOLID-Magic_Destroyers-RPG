945e86d6ecbef65a3866378037aeb5242aa7d8e8
doljpo <dol.jpo@hotmail.com>
2020-05-24 01:16:30 -0300

    Removed unused code from 'Program' class

--- MagicDestroyers/MagicDestroyers/Program.cs
+++ MagicDestroyers/MagicDestroyers/Program.cs
@@ -6,9 +6,6 @@ namespace MagicDestroyers
     {
         static void Main(string[] args)
         {
-            Assassin ninja = new Assassin();
-            Assassin ninja2 = new Assassin("White Ninja", 10);
-            Assassin ninja3 = new Assassin("Red Ninja", 13, 999);
         }
     }
 }
